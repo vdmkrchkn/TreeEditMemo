b09d2bf5fb6ee39373e2b424be249d3965e50cfe
vdmkrchkn <vdmkrchkn@gmail.com>
2015-12-07 21:08:00 +0300

    Добавлены файл перевода, ресурсов и входных данных

---
+++ translations/TreeEditMemo_ru_RU.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>About_Dialog</name>
+    <message>
+        <location filename="about_dialog.ui" line="14"/>
+        <source>About program</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="about_dialog.ui" line="32"/>
+        <source>Family Editor</source>
+        <translation>Редактор &quot;Семья&quot;</translation>
+    </message>
+    <message>
+        <location filename="about_dialog.ui" line="39"/>
+        <source>© Kurochkin Vadim</source>
+        <translation>© Курочкин Вадим</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation type="vanished">Главное окно</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <source>&amp;Window</source>
+        <translation>О&amp;кно</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <source>C&amp;lose All</source>
+        <translation>&amp;Закрыть все окна</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>Close all windows</source>
+        <translation>Закрыть все окна</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <source>Ctrl+W</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <location filename="mainwindow.cpp" line="46"/>
+        <source>Open data file</source>
+        <translation>Открыть файл данных</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="78"/>
+        <source>Exit this application</source>
+        <translation>Выйти из приложения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="81"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="86"/>
+        <source>&amp;About</source>
+        <translation>О &amp;программе</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <source>Show the About  project message</source>
+        <translation>Показать сообщение о программе</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="92"/>
+        <source>F1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="mainwindow.cpp" line="12"/>
+        <source>Family Edit</source>
+        <translation>Редактор &quot;Семья&quot;</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="26"/>
+        <source>Translation</source>
+        <translation>Перевод</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="26"/>
+        <source>Not loaded</source>
+        <translation>Не загружен</translation>
+    </message>
+    <message>
+        <source>Open data file</source>
+        <translation type="vanished">Открыть файл данных</translation>
+    </message>
+</context>
+</TS>
